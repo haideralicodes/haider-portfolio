d5b80e388e36a430528421d7face5450f9bd52e6
Haider Ali <haiderali920687@gmail.com>
2025-05-10 11:25:08 +0500

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
           <div className="gap-2 flex justify-between">
             <div className="flex-col flex flex-1 space-y-1.5">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                Hi, I'm {DATA.name.split(" ")[0]} 👋
+                Hi, I&apos;m {DATA.name.split(" ")[0]} 👋
               </h1>
               <p className="max-w-[600px] md:text-xl">
                 {DATA.description}
